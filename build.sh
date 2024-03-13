93ce07b2071662af8576f3c9fea19a0e335b80a9
dahua.xu <dahua.xu@neardi.com>
2024-03-13 17:24:27 +0800

    Fixed: build error.
    Signed-off-by: dahua.xu <dahua.xu@neardi.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # please below command to install gcc first, and then install opencv
 # sudo apt install build-essential libopencv-dev
 #aarch64-linux-gnu-g++ -std=c++11  -O3  -I/usr/include/opencv4  -L/usr/lib/aarch64-linux-gnu -lX11 main.cpp -o main
-aarch64-linux-gnu-g++ -std=c++11 -O3  -o n4camera main.cpp -lpthread -lopencv_core -lgstopencv -lopencv_videoio -lopencv_imgcodecs -lopencv_highgui -lopencv_imgproc -I/usr/include/opencv4  -L/usr/lib/aarch64-linux-gnu -L/usr/local/lib/aarch64-linux-gnu/gstreamer-1.0
+aarch64-linux-gnu-g++ -std=c++11 -O3  -o n4camera main.cpp -lpthread -lopencv_core -lopencv_videoio -lopencv_imgcodecs -lopencv_highgui -lopencv_imgproc -I/usr/include/opencv4  -L/usr/lib/aarch64-linux-gnu -L/usr/local/lib/aarch64-linux-gnu/gstreamer-1.0
